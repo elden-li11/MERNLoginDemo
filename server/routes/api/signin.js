eb2ce181eab50e1278e2b37907e1251ce9c49ccd
Elden Li <lielden@yahoo.com>
2019-01-28 11:28:16 -0800

    complete backend (sort of)

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -193,14 +193,28 @@ module.exports = (app) => {
     });
 
     app.get('/api/account/logout', (req, res, next) => {
-        const {query} = req;
-        const {token} = query;
+        const { query } = req;
+        const { token } = query;
 
         UserSession.findOneAndUpdate({
             _id: token,
             isDeleted: false
         }, {
-
+            $set: {
+                isDeleted: true
+            }
+        }, null, (err, sessions) => {
+            if (err) {
+                console.log(err);
+                return res.send({
+                    success: false,
+                    message: "Error: Server error"
+                });
+            }
+            return res.send({
+                success: true,
+                message: "Good"
+            });
         });
     });
 };
