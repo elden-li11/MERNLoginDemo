dbda24abe03234f9da9edbf243eecc8750ec036e
Elden Li <lielden@yahoo.com>
2019-01-30 17:04:25 -0800

    finished signin, sign up, and log out

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -3,22 +3,9 @@ const UserSession = require('../../models/UserSession');
 
 
 module.exports = (app) => {
-    // app.get('/api/counters', (req, res, next) => {
-    //   Counter.find()
-    //     .exec()
-    //     .then((counter) => res.json(counter))
-    //     .catch((err) => next(err));
-    // });
-  
-    // app.post('/api/counters', function (req, res, next) {
-    //   const counter = new Counter();
-  
-    //   counter.save()
-    //     .then(() => res.json(counter))
-    //     .catch((err) => next(err));
-    // });
     app.post('/api/account/signup', (req, res, next) => {
         const {body} = req;
+        console.log('body: ', body);
         const {
             firstName,
             lastName,
